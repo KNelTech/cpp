a25cb61a23706ce317643cf96e304955de2d5659
lt3 <k.nelsontech@gmail.com>
2022-12-14 02:27:46 -0600

    cppinitial

---
+++ lettertomeCPP.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main() 
+{
+  std::cout << "Dear Self,\n";
+  std::cout << "I would like to be a good programmer,\n";
+  std::cout << "today is the 14th of december in 2022\n";
+}
+
+/*
+
+
+so using /n making a newline
+
+So to declare an integer variable called score,
+we need to write:
+
+int score;
+
+The int is the type of the variable.
+The score is the name of the variable.
+The ; is how we end a statement.
+
+In C++, variable names consist only of upper/lower case letters,
+digits, and/or underscores.
+Note: C++ is known as a strongly typed language.
+If you try to give an integer value a decimal number,
+you are going to get unexpected results, warnings, or errors.
+
+
+Compilation and execution using the terminal.
+g++ hello.cpp -o hello
+./hello
+Single line comments can be created using //.
+Multi-line comments can be created using /*/ /*/.
+*/
+/*
+int: integer numbers
+double: floating-point numbers
+char: individual characters
+string: a sequence of characters
+bool: true/false values
+*/
